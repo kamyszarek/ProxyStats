cf3f93b9f2c6c5f3c15d2e91bc021b1eaa83b02b
kamyszarek <tekstowy.arek@gmail.com>
2023-12-31 10:50:36 +0100

    master: Code cleanup v2

--- arkaserver/src/main/resources/static/homepage/homepage-controller.js
+++ arkaserver/src/main/resources/static/homepage/homepage-controller.js
@@ -2,47 +2,4 @@ angular.module('myApp')
     .controller('HomeController', function ($scope, $http) {
         var $this = this;
 
-        $scope.startProxy = function () {
-            var endpoint = '/api/start';
-            var proxyPort = 5555;
-            var params = {
-                proxyPort: proxyPort
-            };
-            $http({
-                method: 'GET',
-                url: endpoint,
-                params: params,
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-            .then(function (response) {
-                console.log('Odpowiedź z serwera:', response.data);
-            })
-            .catch(function (error) {
-                console.error('Błąd podczas wysyłania żądania:', error);
-            });
-        };
-
-        $scope.stopProxy = function () {
-            var endpoint = '/api/stop';
-            var proxyPort = 5555;
-            var params = {
-                proxyPort: proxyPort
-            };
-            $http({
-                method: 'GET',
-                url: endpoint,
-                params: params,
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-            .then(function (response) {
-                console.log('Odpowiedź z serwera:', response.data);
-            })
-            .catch(function (error) {
-                console.error('Błąd podczas wysyłania żądania:', error);
-            });
-        };
     });
